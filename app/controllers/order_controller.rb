6d726044dd39a1a00718cd66b73715131909951c
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-02-22 10:48:08 -0700

    Filter products by category

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,7 +1,18 @@
 class OrderController < ApplicationController
   def new
     @categories_by_levels = Category.by_levels
+
     @products = Product.all
+
+    category_map = Category.all.map { |category| [category.id, category] }.to_h
+
+    @category_ancestor_map = Category.ancestor_map
+
+    @products_with_ancestors = @products.map do |product|
+      [product, product.all_ancestor_category_ids(category_map)]
+    end
+
+    render :new
   end
 
   def create
--- app/javascript/order.js
+++ app/javascript/order.js
@@ -1,9 +1,95 @@
-const categories = $('.js-category');
+function getCategories() {
+  return document.querySelectorAll(".js-category");
+}
+function getProducts() {
+  return document.querySelectorAll(".js-product");
+}
 
-categories.click((event) => {
+function addClickListener(collection, callback) {
+  collection.forEach((element) => element.addEventListener("click", callback));
+}
+
+function deactivateAllCategories() {
+  getCategories().forEach((category) =>
+    category.classList.remove("js-active-category")
+  );
+}
+
+function setActiveCategory(category) {
+  category.classList.add("js-active-category");
+}
+
+function getActiveCategory() {
+  return document.querySelector(".js-active-category");
+}
+
+function getCategoryId(category) {
+  if (category === null) return null;
+
+  return parseInt(category.dataset.categoryId);
+}
+
+function getProductCategories(product) {
+  return JSON.parse(product.dataset.ancestors);
+}
+
+function activateProduct(product) {
+  product.classList.remove("js-active-product");
+  product.parentElement.classList.remove("d-none");
+}
+
+function deactivateProduct(product) {
+  product.classList.add("js-active-product");
+  product.parentElement.classList.add("d-none");
+}
+
+function deactivateAllProducts() {
+  getProducts().forEach(deactivateProduct);
+}
+
+function activateAllProducts() {
+  getProducts().forEach(activateProduct);
+}
+
+function setActiveProducts() {
+  const activeCategoryId = getCategoryId(getActiveCategory());
+
+  // If no category is selected, all products are set to active
+  if (activeCategoryId === null) {
+    activateAllProducts();
+    return;
+  }
+
+  // Set products who have the active category id in their ancestors as active
+  getProducts().forEach((product) => {
+    const categories = getProductCategories(product);
+    if (categories.includes(activeCategoryId)) {
+      activateProduct(product);
+    } else {
+      deactivateProduct(product);
+    }
+  });
+}
+
+function handleCategoryClick(event) {
   const category = event.currentTarget;
-  const id = category.dataset['category_id'];
 
-  console.log(`Category #${id} was clicked!`)
-});
+  deactivateAllCategories();
+  setActiveCategory(category);
 
+  deactivateAllProducts();
+  setActiveProducts();
+}
+
+function handleProductClick(event) {
+  const product = event.currentTarget;
+  const id = product.dataset.productId;
+  const myCategories = JSON.parse(product.dataset.ancestors);
+
+  console.log(`Product #${id} was clicked!`);
+  console.log(myCategories);
+}
+
+setActiveProducts();
+addClickListener(getCategories(), handleCategoryClick);
+addClickListener(getProducts(), handleProductClick);
