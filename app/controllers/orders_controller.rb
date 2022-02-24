a984aed7d301a54c03fc71c8257d83078e413838
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-02-24 13:23:33 -0700

    Create payment when creating order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,18 +8,22 @@ class OrdersController < ApplicationController
 
   def create
     order = Order.new
-    items = order_params.map do |item_params|
+    items = order_params[:items].map do |item_params|
       order_item = OrderItem.new(item_params)
       order_item.order = order
       order_item
     end
     order.order_items = items
 
-    if order.save
+    payment = Payment.new(order_params[:payment])
+    payment.order = order
+    payment.amount = order.total_price
+
+    if order.save && payment.save
       flash[:alerts] = ['Order placed successfully!']
       redirect_to new_order_url
     else
-      flash[:errors] = order.errors.full_messages
+      flash[:errors] = order.errors.full_messages + payment.errors.full_messages
       redirect_to new_order_url
     end
   end
@@ -27,8 +31,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).require(:items).map do |item_params|
-      item_params.permit(:product_id, :quantity)
-    end
+    params.require(:order).permit(items: %i[product_id quantity],
+                                  payment: [:payment_type])
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -12,27 +12,28 @@
 #
 class OrderItem < ApplicationRecord
   before_validation :ensure_unit_price
-  
+
   validates :unit_price, :quantity,
-    presence: true,
-    numericality: { greater_than_or_equal_to: 0}
+            presence: true,
+            numericality: true
 
   belongs_to :product,
-    inverse_of: :order_items
+             inverse_of: :order_items
 
   belongs_to :order,
-    inverse_of: :order_items
-  
+             inverse_of: :order_items
+
   def total_price
+    ensure_unit_price
     unit_price * quantity
   end
-  
+
   private
 
   # If unit price hasn't been set, set it equal to the product's price
   def ensure_unit_price
-    return unless self.product
-    
+    return unless product
+
     self.unit_price ||= product.price
   end
 end
