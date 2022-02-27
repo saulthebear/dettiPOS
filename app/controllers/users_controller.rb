12448ac22594051a19d0dac19a50d8c6c83b427b
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-02-26 21:13:00 -0700

    Add users routes views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,30 @@
 class UsersController < ApplicationController
   before_action :require_admin, except: %i[new create]
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to :root, notice: ['Account created sucessfully.']
+    else
+      redirect_to new_user_url, status: :unprocessable_entity, alert: user.errors.full_messages
+    end
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+  end
 
-  def index; end
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password)
+  end
 end
